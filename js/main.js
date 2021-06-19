f7ad4621850ce41f618f766b2af12c961f4311a8
alriana <alriana.dev@gmail.com>
2021-06-19 21:45:47 +0700

    fix empty slides and next slides

--- js/main.js
+++ js/main.js
@@ -1,43 +1,55 @@
-var slideIndex = 1;
-showAllSlides(slideIndex);
+showAllSlides();
 
 function showAllSlides(){
     const portos = document.getElementsByClassName("portofolio");
-    console.log(portos)
     for (const p of portos) {
         showSlides(p.id, 1)
     }
 }
 
-function showSlides(portoId, n){
+function showSlides(portoId, idx){
+    var slideIndex = idx;
     var i;
-    var porto = porto1 = document.getElementById(portoId)
+    var porto = document.getElementById(portoId)
     var slides = porto.getElementsByClassName("mySlides");
     var dots = porto.getElementsByClassName("dot");
+    
+    if(slides.length){
+        if (idx > slides.length){
+            slideIndex = 1;
+        }
+        if (idx < 1){
+            slideIndex = slides.length;
+        }
 
-    if (n > slides.length){
-        slideIndex = 1;
-    }
-    if (n < 1){
-        slideIndex = slides.length;
-    }
+        for (i=0; i<slides.length; i++){
+            slides[i].style.display = "none";
+        }
 
-    for (i=0; i<slides.length; i++){
-        slides[i].style.display = "none";
-    }
+        for (i=0; i<dots.length; i++){
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
 
-    for (i=0; i<dots.length; i++){
-        dots[i].className = dots[i].className.replace(" active", "");
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
     }
-
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
 }
 
-function currentSlide(portoId, n){
-    showSlides(portoId, slideIndex = n);
+function currentSlide(portoId, idx){
+    showSlides(portoId, idx);
 }
 
-function plusSlides(portoId,n){
-    showSlides(portoId, slideIndex += n);
+function plusSlides(portoId, n){
+    var idx = 1;
+    var porto = document.getElementById(portoId)
+    var slides = porto.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        if(slides[i].style.display == "block"){
+            idx = i+1;
+            break;
+        }
+    }
+    showSlides(portoId, idx+=n);
 }
+
+// document.getElementById("porto1").getElementsByClassName("mySlides");
